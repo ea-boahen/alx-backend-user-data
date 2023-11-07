1d2a753e64719d132c73bad4ab7e2312eed2f7f2
ea-boahen <ea.boahen2015@gmail.com>
2023-11-07 23:59:10 +0000

    3. Auth class correction

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -4,6 +4,7 @@
 from flask import request
 from typing import List, TypeVar
 
+
 class Auth:
     """Authentication template"""
 
@@ -17,14 +18,13 @@ class Auth:
         """
 
     def require_auth(self, path: str, exclude_paths: List[str]) -> bool:
-        #This method will be implemented later, for now its returning False
+        # This method will be implemented later, for now its returning False
         return False
 
     def authorization_header(self, request=None) -> str:
-        #This method will be implemented later, for now its returning None
+        # This method will be implemented later, for now its returning None
         return None
 
     def current_user(self, request=None) -> TypeVar('User'):
-        #This method will be implemented later, for now its returning None
+        # This method will be implemented later, for now its returning None
         return None
-
