b18cac332c58c49cbc45476e6aecdc32e1173bd3
ea-boahen <ea.boahen2015@gmail.com>
2023-11-08 00:12:31 +0000

    auth correction

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -18,13 +18,38 @@ class Auth:
         """
 
     def require_auth(self, path: str, exclude_paths: List[str]) -> bool:
-        # This method will be implemented later, for now its returning False
+        """
+            Require the auth
+
+            Args:
+                path: path to authenticate
+                excluded_paths: list of excluded path to authenticate
+
+            Return:
+                True if is authenticated otherwise false
+        """
         return False
 
     def authorization_header(self, request=None) -> str:
-        # This method will be implemented later, for now its returning None
+        """
+            Look the headers
+
+            Args:
+                request: Look the autthorization
+
+            Return:
+                The authorization header or None
+        """
         return None
 
     def current_user(self, request=None) -> TypeVar('User'):
-        # This method will be implemented later, for now its returning None
+        """
+            Look current user
+            Args:
+
+                request: Look the reques user
+
+            Return:
+                The user
+        """
         return None
